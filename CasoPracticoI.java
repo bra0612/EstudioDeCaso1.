ce9947f8ce8132358d91de19bdbbeab0e669ea7c
Brayan Molina Pizarro <molinapizarrobrayan@gmail.com>
2025-10-22 20:49:39 -0600

    Add files via upload

---
+++ CasoPracticoI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casopracticoi;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brayan Molina
+ */
+public class CasoPracticoI {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    }
+
+
+    
+    
+
---
+++ Facturas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package casopracticoi;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brayan Molina
+ */
+public class Facturas {
+
+    private String datos;
+    private String nombreCliente;
+    private int cedulaCliente;
+    private int codigoFactura;
+    private double montoFactura;
+    private int mes;
+
+    String nombreClientes = "";
+
+
+}
+    
+    
+    
